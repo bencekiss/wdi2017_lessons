e9d16de24a23f83d953a95c5dbb9cbaec4978dad
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-06 11:56:01 -0500

    remove useless views from sessions

--- 21-sessions-and-authentication/authentication/config/routes.rb
+++ 21-sessions-and-authentication/authentication/config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # get 'sessions/new'
 
-  get 'sessions/create'
+  # get 'sessions/create'
 
-  get 'sessions/destroy'
+  # get 'sessions/destroy'
 
 #  get 'users/new'
 
 #  get 'users/create'
 
-
+  resource :sessions, only: %i(new create destroy)
   resource :users, only: %i(new create) # %i is the shorthand of an array of symbols
   # resource :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
