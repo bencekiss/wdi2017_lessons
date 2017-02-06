13c59a33e02c78b58c9bdbe019531cec4d329c0d
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-06 11:19:30 -0500

    remove useless routes and add resource :users, only: %i(new create)

--- 21-sessions-and-authentication/authentication/config/routes.rb
+++ 21-sessions-and-authentication/authentication/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
+#  get 'users/new'
 
-  get 'users/create'
+#  get 'users/create'
 
+
+  resource :users, only: %i(new create) # %i is the shorthand of an array of symbols
+  # resource :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
