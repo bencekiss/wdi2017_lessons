19fb4c657f6b3aabbcce71b4f09b867e8e3cf0e4
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-06 11:52:13 -0500

    Generate sessions controller and added validations for new

--- 21-sessions-and-authentication/authentication/app/controllers/users_controller.rb
+++ 21-sessions-and-authentication/authentication/app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      # flash is for the next request.
+      # if there is a redirect, never use flash.now, only flash
+      redirect_to root_url
+    else
+      flash.now[:error] = 'Sorry, try again!'
+      # flash.now is for the actual request, and that only.
+      # only use flash.now, when there is a render, not a redirect.
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- 21-sessions-and-authentication/authentication/config/routes.rb
+++ 21-sessions-and-authentication/authentication/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
 #  get 'users/new'
 
 #  get 'users/create'
@@ -7,4 +13,6 @@ Rails.application.routes.draw do
   resource :users, only: %i(new create) # %i is the shorthand of an array of symbols
   # resource :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#new'
 end
